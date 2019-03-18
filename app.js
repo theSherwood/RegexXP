7ea75a428a9d506b3e4f0071e4a42f527972e7de
admls <adam.the.sherwood@gmail.com>
2019-03-18 18:20:52 +0000

    adds handlebars and global user variable

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const session = require('express-session');
 const passport = require('passport');
+const exphbs = require('express-handlebars');
 
 // Load Models
 require('./models/User');
@@ -38,6 +39,16 @@ app.use(session({
 // Passport Middleware
 app.use(passport.initialize());
 app.use(passport.session());
+
+// Set Global Vars
+app.use((req, res, next) => {
+  res.locals.user = req.user || null;
+  next();
+});
+
+// Express-handlebars Middleware
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
 
 // Use Routes
 app.use('/auth', auth);
