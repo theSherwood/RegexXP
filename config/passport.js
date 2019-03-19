8628852eab36257707e53941b47a49097c06a410
admls <adam.the.sherwood@gmail.com>
2019-03-19 00:16:07 +0000

    adds local and github auth strategies to passport

--- config/passport.js
+++ config/passport.js
@@ -1,10 +1,33 @@
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const LocalStrategy = require('passport-local').Strategy;
+const GitHubStrategy = require('passport-github2').Strategy;
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
 // Load user model
 const User = mongoose.model('users');
 
 module.exports = function (passport) {
+  passport.use(new LocalStrategy({ usernameField: 'email' }, (email, password, done) => {
+    // Match user
+    User.findOne({
+      email: email
+    }).then(user => {
+      if (!user) {
+        return done(null, false, { message: 'No User Found' });
+      }
+      // Match password
+      bcrypt.compare(password, user.password, (err, isMatch) => {
+        if (err) throw err;
+        if (isMatch) {
+          return done(null, user);
+        } else {
+          return done(null, false, { message: 'Password Incorrect' });
+        }
+      })
+    })
+  }));
+
   passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
@@ -14,8 +37,7 @@ module.exports = function (passport) {
     (accessToken, refreshToken, profile, done) => {
       const newUser = {
         googleID: profile.id,
-        firstName: profile.name.givenName,
-        lastName: profile.name.familyName,
+        displayName: profile.name.givenName || profile.name.familyName,
         email: profile.emails[0].value,
         image: profile.photos[0].value
       }
@@ -36,6 +58,36 @@ module.exports = function (passport) {
       });
     })
   );
+
+  passport.use(new GitHubStrategy({
+    clientID: process.env.GITHUB_CLIENT_ID,
+    clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    callbackURL: "/auth/github/callback",
+    proxy: true
+  },
+    (accessToken, refreshToken, profile, done) => {
+      const newUser = {
+        githubId: profile.id,
+        displayName: profile.displayName || profile.username,
+        email: profile._json.email,
+        image: profile.photos[0].value
+      }
+
+      User.findOne({
+        githubId: profile.id
+      }).then(user => {
+        if (user) {
+          // Return user
+          done(null, user);
+        } else {
+          // Create user
+          new User(newUser)
+            .save()
+            .then(user => done(null, user));
+        }
+      });
+    }
+  ));
 
   passport.serializeUser((user, done) => {
     done(null, user.id);
