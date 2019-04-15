a455f8e7736cfb919c0c2765d7745e77bfa168b1
admls <adam.the.sherwood@gmail.com>
2019-04-15 09:51:13 +0100

    init Header, Footer, Tester

--- client/src/App.js
+++ client/src/App.js
@@ -3,11 +3,17 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import "./App.css";
 
 import Header from "./components/layout/Header";
+import Footer from "./components/layout/Footer";
+import Tester from "./components/tester/Tester";
 
 export default function App() {
   return (
-    <div className="App">
-      <h1>App</h1>
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <Route exact path="/" component={Tester} />
+        <Footer />
+      </div>
+    </Router>
   );
 }
---
+++ client/src/components/tester/Tester.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Tester() {
+  return (
+    <div>
+      <h1>Tester</h1>
+    </div>
+  );
+}
