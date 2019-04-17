5a1d09fac8d6b3cab4ca688c4008025d6874e51a
admls <adam.the.sherwood@gmail.com>
2019-04-17 22:06:51 +0100

    removes express-handlebars and static path

--- server.js
+++ server.js
@@ -1,40 +1,48 @@
-const express = require('express');
-const path = require('path');
-const mongoose = require('mongoose');
-const session = require('express-session');
-const passport = require('passport');
-const exphbs = require('express-handlebars');
-const bodyParser = require('body-parser');
+const express = require("express");
+const path = require("path");
+const mongoose = require("mongoose");
+const session = require("express-session");
+const passport = require("passport");
+const exphbs = require("express-handlebars");
+const bodyParser = require("body-parser");
 
 // Load Models
-require('./models/User');
+require("./models/User");
 
 // Load Routes
-const auth = require('./routes/auth');
-const index = require('./routes/index');
-const puzzle = require('./routes/puzzle');
+// const auth = require('./routes/auth');
+// const index = require('./routes/index');
+// const puzzle = require('./routes/puzzle');
+const auth = require("./routes/api/auth");
+const index = require("./routes/index");
+const puzzle = require("./routes/api/challenges");
 
 // Config Environment Vars
-require('dotenv').config();
+if (process.env.NODE_ENV !== production) {
+  require("dotenv").config();
+}
 
 // Passport Config
-require('./config/passport')(passport);
+require("./config/passport")(passport);
 
 // Mongoose Connect
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true
-})
-.then(() => console.log('MongoDB Connected'))
-.catch(err => console.log(err));
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true
+  })
+  .then(() => console.log("MongoDB Connected"))
+  .catch(err => console.log(err));
 
 const app = express();
 
 // Express session Middleware
-app.use(session({
-  secret: 'secret',
-  resave: false,
-  saveUninitialized: false
-}));
+app.use(
+  session({
+    secret: "secret",
+    resave: false,
+    saveUninitialized: false
+  })
+);
 
 // Passport Middleware
 app.use(passport.initialize());
@@ -44,26 +52,13 @@ app.use(passport.session());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-// Set Global Vars
-app.use((req, res, next) => {
-  res.locals.user = req.user || null;
-  next();
-});
-
-// Express-handlebars Middleware
-app.engine('handlebars', exphbs({defaultLayout: 'main'}));
-app.set('view engine', 'handlebars');
-
 // Use Routes
-app.use('/', index);
-app.use('/auth', auth);
-app.use('/puzzle', puzzle);
-
-// Set Static Folder
-app.use(express.static(path.join(__dirname, 'public')));
+app.use("/", index);
+app.use("/auth", auth);
+app.use("/puzzle", puzzle);
 
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
-  console.log(`Server started on port ${port}`)
+  console.log(`Server started on port ${port}`);
 });
