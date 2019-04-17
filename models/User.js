e937a069b951af4379e8c1c30d1e96f5850572ff
admls <adam.the.sherwood@gmail.com>
2019-04-17 22:05:50 +0100

    exports model

--- models/User.js
+++ models/User.js
@@ -1,4 +1,4 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 // Create Schema
@@ -28,4 +28,4 @@ const UserSchema = new Schema({
 });
 
 // Create collection and add schema
-mongoose.model('users', UserSchema);
+module.exports = User = mongoose.model("users", UserSchema);
