29f69a1cdae256fc381815f664f9c81ef5eb67c7
admls <adam.the.sherwood@gmail.com>
2019-04-20 18:57:03 +0100

    adds getChallenge (by id)

--- client/src/actions/challengeActions.js
+++ client/src/actions/challengeActions.js
@@ -1,10 +1,15 @@
 import axios from "axios";
 
-import { ADD_CHALLENGE, GET_ERRORS, GET_CHALLENGES, IS_LOADING } from "./types";
+import {
+  ADD_CHALLENGE,
+  GET_ERRORS,
+  GET_CHALLENGES,
+  GET_CHALLENGE,
+  IS_LOADING
+} from "./types";
 
 // Add Challenge
 export const addChallenge = challengeData => dispatch => {
-  dispatch(setLoading());
   axios
     .post("/api/challenges", challengeData)
     .then(res => {
@@ -23,11 +28,31 @@ export const addChallenge = challengeData => dispatch => {
 
 // Get Challenges
 export const getChallenges = () => dispatch => {
+  dispatch(setLoading());
   axios
     .get("/api/challenges")
     .then(res =>
       dispatch({
         type: GET_CHALLENGES,
+        payload: res.data
+      })
+    )
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+};
+
+// Get challenge by id
+export const getChallenge = challengeId => dispatch => {
+  dispatch(setLoading());
+  axios
+    .get(`/api/challenges/${challengeId}`)
+    .then(res =>
+      dispatch({
+        type: GET_CHALLENGE,
         payload: res.data
       })
     )
