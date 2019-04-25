06054b757d4d09cc7a709b301d94dc82c07769af
admls <adam.the.sherwood@gmail.com>
2019-04-25 19:38:13 +0100

    fixes loading issue, brings in Solutions, Comments

--- client/src/components/thread/Thread.js
+++ client/src/components/thread/Thread.js
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useState, useEffect, Fragment } from "react";
 import { connect } from "react-redux";
 
 import Spinner from "../common/Spinner";
+import Solutions from "./Solutions";
+import Comments from "./Comments";
 
 import {
   getCommentsToChallenge,
@@ -9,42 +11,71 @@ import {
 } from "../../actions/challengeActions";
 
 function Thread(props) {
-  const { challengeId } = props;
+  const { challengeId, comments, solutions } = props;
+  const [showSolutions, setShowSolutions] = useState(false);
+  const [showComments, setShowComments] = useState(false);
+  const [fetching, setFetching] = useState(false);
 
   const seeComments = () => {
     props.getCommentsToChallenge(challengeId);
+    setShowComments(true);
+    setShowSolutions(false);
+    setFetching(true);
   };
 
   const seeSolutions = () => {
     props.getSolutions(challengeId);
+    setShowComments(false);
+    setShowSolutions(true);
+    setFetching(true);
   };
 
+  useEffect(() => {
+    setFetching(false);
+  }, [comments, solutions]);
+
   let contents;
+  if (fetching) {
+    contents = <Spinner size="40vmin" additionalClasses="text-warning" />;
+  } else if (showComments) {
+    contents = <Comments comments={comments} />;
+  } else if (showSolutions) {
+    contents = <Solutions solutions={solutions} />;
+  }
   return (
-    <div className="row">
-      <button
-        onClick={seeComments}
-        type="button"
-        className="btn btn-secondary btn-sm mt-3 col"
-      >
-        See comments
-      </button>
-      <div className="col" />
-      <button
-        onClick={seeSolutions}
-        type="button"
-        className="btn btn-secondary btn-sm mt-3 col"
-      >
-        See solutions
-      </button>
+    <Fragment>
+      <div className="container">
+        <div className="row mb-3">
+          <button
+            onClick={seeComments}
+            type="button"
+            className={
+              "btn btn-secondary btn-sm mt-3 col " +
+              (showComments ? "btn-warning" : "btn-secondary")
+            }
+          >
+            See comments
+          </button>
+          <button
+            onClick={seeSolutions}
+            type="button"
+            className={
+              "btn btn-secondary btn-sm mt-3 col " +
+              (showSolutions ? "btn-warning" : "btn-secondary")
+            }
+          >
+            See solutions
+          </button>
+        </div>
+      </div>
       {contents}
-    </div>
+    </Fragment>
   );
 }
 
 const mapStateToProps = state => ({
-  comments: state.comments,
-  solutions: state.solutions
+  comments: state.challenge.comments,
+  solutions: state.challenge.solutions
 });
 
 export default connect(
