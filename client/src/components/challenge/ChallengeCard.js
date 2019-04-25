7e8d2b354c79ace3eebaf7ea9260f12a980ad929
admls <adam.the.sherwood@gmail.com>
2019-04-25 14:38:01 +0100

    uses HighlightDiv

--- client/src/components/challenge/ChallengeCard.js
+++ client/src/components/challenge/ChallengeCard.js
@@ -1,43 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
-import HighlightMark from "../common/HighlightMark";
 import { Link } from "react-router-dom";
-// import {connect} from 'react-redux'
+import HighlightDiv from "../common/HighlightDiv";
 
 export default function ChallengeCard(props) {
   const { highlightTemplate, description, title, user, _id } = props.challenge;
-
-  const targetText = JSON.parse(highlightTemplate).map((text, index) =>
-    Array.isArray(text) ? (
-      <HighlightMark
-        key={index}
-        highlightText={text}
-        highlightColor={"#ffc107"}
-      />
-    ) : (
-      text
-    )
-  );
+  const [highlightArray] = useState(JSON.parse(highlightTemplate));
 
   return (
-    <div className="container">
-      <div className="card mb-2">
-        <div className="card-content">
-          <div className="container p-4" style={{ fontFamily: "monospace" }}>
-            <div className="clearfix">
-              <h4 className="float-left">{title}</h4>
-              <Link
-                className="float-right btn btn-warning btn-sm"
-                to={`/challenges/${_id}`}
-              >
-                Attempt
-              </Link>
-            </div>
-            <p className="lead">{user.handle}</p>
-            <p className="text-muted">{description}</p>
-            <div className="container">{targetText}</div>
+    <div className="card mb-2">
+      <div className="card-content">
+        <div className="container p-4" style={{ fontFamily: "monospace" }}>
+          <div className="clearfix">
+            <h4 className="float-left">{title}</h4>
+            <Link
+              className="float-right btn btn-warning btn-sm"
+              to={`/challenges/${_id}`}
+            >
+              Attempt
+            </Link>
           </div>
+          <p className="lead">{user.handle}</p>
+          <p className="text-muted">{description}</p>
         </div>
+      </div>
+      <div
+        className="container mb-3"
+        style={{
+          overflowY: "auto",
+          maxHeight: "60vh"
+        }}
+      >
+        <HighlightDiv
+          highlightColor="#ffc107"
+          highlightArray={highlightArray}
+        />
       </div>
     </div>
   );
