1485ac2d0cad7b094544755bc92c43b840c32d8e
admls <adam.the.sherwood@gmail.com>
2019-04-25 14:37:02 +0100

    styling improvements

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -23,36 +23,34 @@ function Login(props) {
   };
 
   return (
-    <div className="container">
-      <div className="card" style={{ backgroundColor: "#35a7ff" }}>
-        <div className="card-content">
-          <div className="container p-4">
-            <form onSubmit={onSubmit}>
-              <div className="form-group">
-                <label htmlFor="email">Email</label>
-                <input
-                  name="email"
-                  type="email"
-                  className="form-control"
-                  value={formData.email}
-                  onChange={onChange}
-                />
-              </div>
-              <div className="form-group">
-                <label htmlFor="password">Password</label>
-                <input
-                  name="password"
-                  type="password"
-                  className="form-control"
-                  value={formData.password}
-                  onChange={onChange}
-                />
-              </div>
-              <button type="submit" className="btn btn-warning mt-2">
-                Login
-              </button>
-            </form>
-          </div>
+    <div className="card">
+      <div className="card-content">
+        <div className="container p-4">
+          <form onSubmit={onSubmit}>
+            <div className="form-group">
+              <label htmlFor="email">Email</label>
+              <input
+                name="email"
+                type="email"
+                className="form-control"
+                value={formData.email}
+                onChange={onChange}
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="password">Password</label>
+              <input
+                name="password"
+                type="password"
+                className="form-control"
+                value={formData.password}
+                onChange={onChange}
+              />
+            </div>
+            <button type="submit" className="btn btn-warning mt-2">
+              Login
+            </button>
+          </form>
         </div>
       </div>
     </div>
--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -26,56 +26,54 @@ function Register(props) {
 
   const { email, password, password2, handle } = formData;
   return (
-    <div className="container">
-      <div className="card" style={{ backgroundColor: "#35a7ff" }}>
-        <div className="card-content">
-          <div className="container p-4">
-            <form onSubmit={onSubmit}>
-              <div className="form-group">
-                <label htmlFor="handle">Display Name/Handle</label>
-                <input
-                  name="handle"
-                  type="text"
-                  className="form-control"
-                  value={handle}
-                  onChange={onChange}
-                />
-              </div>
-              <div className="form-group">
-                <label htmlFor="email">Email</label>
-                <input
-                  name="email"
-                  type="email"
-                  className="form-control"
-                  value={email}
-                  onChange={onChange}
-                />
-              </div>
-              <div className="form-group">
-                <label htmlFor="password">Password</label>
-                <input
-                  name="password"
-                  type="password"
-                  className="form-control"
-                  value={password}
-                  onChange={onChange}
-                />
-              </div>
-              <div className="form-group">
-                <label htmlFor="password2">Confirm Password</label>
-                <input
-                  name="password2"
-                  type="password"
-                  className="form-control"
-                  value={password2}
-                  onChange={onChange}
-                />
-              </div>
-              <button type="submit" className="btn btn-warning mt-2">
-                Register
-              </button>
-            </form>
-          </div>
+    <div className="card">
+      <div className="card-content">
+        <div className="container p-4">
+          <form onSubmit={onSubmit}>
+            <div className="form-group">
+              <label htmlFor="handle">Display Name/Handle</label>
+              <input
+                name="handle"
+                type="text"
+                className="form-control"
+                value={handle}
+                onChange={onChange}
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="email">Email</label>
+              <input
+                name="email"
+                type="email"
+                className="form-control"
+                value={email}
+                onChange={onChange}
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="password">Password</label>
+              <input
+                name="password"
+                type="password"
+                className="form-control"
+                value={password}
+                onChange={onChange}
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="password2">Confirm Password</label>
+              <input
+                name="password2"
+                type="password"
+                className="form-control"
+                value={password2}
+                onChange={onChange}
+              />
+            </div>
+            <button type="submit" className="btn btn-warning mt-2">
+              Register
+            </button>
+          </form>
         </div>
       </div>
     </div>
