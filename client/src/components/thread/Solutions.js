57c008dd9cd09efe464eaa1cee0aac624cade889
admls <adam.the.sherwood@gmail.com>
2019-05-12 00:06:42 +0100

    adds a message if there are no solutions

--- client/src/components/thread/Solutions.js
+++ client/src/components/thread/Solutions.js
@@ -4,17 +4,21 @@ export default function Solutions(props) {
   const { solutions } = props;
   return (
     <Fragment>
-      {solutions.map((solution, i) => (
-        <div key={i} className="card mt-2" style={{ fontSize: "12px" }}>
-          <div className="card-content">
-            <div className="container p-2">
-              <h5>{solution.regex}</h5>
-              <p className="lead">{solution.handle}</p>
-              <p className="text-muted">{solution.date}</p>
+      {solutions.length ? (
+        solutions.map((solution, i) => (
+          <div key={i} className="card mt-2" style={{ fontSize: "12px" }}>
+            <div className="card-content">
+              <div className="container p-2">
+                <h5>{solution.regex}</h5>
+                <p className="lead">{solution.handle}</p>
+                <p className="text-muted">{solution.date}</p>
+              </div>
             </div>
           </div>
-        </div>
-      ))}
+        ))
+      ) : (
+        <p className="text-center">This challenge hasn't been solved yet.</p>
+      )}
     </Fragment>
   );
 }
