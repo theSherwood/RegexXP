dc1f64109b55690f471404f0f7b12b65e89041ae
admls <adam.the.sherwood@gmail.com>
2019-05-12 02:07:32 +0100

    adds message if there are no comments

--- client/src/components/thread/Comments.js
+++ client/src/components/thread/Comments.js
@@ -5,17 +5,23 @@ export default function Comments(props) {
 
   return (
     <Fragment>
-      {comments.map((comment, i) => (
-        <div key={i} className="card mt-2" style={{ fontSize: "12px" }}>
-          <div className="card-content">
-            <div className="container p-2">
-              <h6>{comment.handle}</h6>
-              <p className="lead">{comment.text}</p>
-              <p className="text-muted">{comment.date}</p>
+      {comments.length ? (
+        comments.map((comment, i) => (
+          <div key={i} className="card mt-2" style={{ fontSize: "12px" }}>
+            <div className="card-content">
+              <div className="container p-2">
+                <h6>{comment.handle}</h6>
+                <p className="lead">{comment.text}</p>
+                <p className="text-muted">{comment.date}</p>
+              </div>
             </div>
           </div>
-        </div>
-      ))}
+        ))
+      ) : (
+        <p className="text-center">
+          This challenge doesn't have any comments yet.
+        </p>
+      )}
     </Fragment>
   );
 }
